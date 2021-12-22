c8f12cae2a2632de644c09bcf69569de04b01fef
Tarık Yeşilçimen <tarikyesilcimenn@gmail.com>
2021-12-22 11:32:52 +0300

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,39 @@
 <template>
-   <div>
-       <h1>LOGIN COMPONENT</h1>
-   </div>
+  <div>
+    <div class="column">
+      <div class="d-flex flex-column  align-items-center">
+        <div id="box" class="row shadow p-3 mb-5 bg-white rounded">
+          <div class="container">
+            <h3>Email</h3>
+
+            <input type="text" />
+            <h3>Password</h3>
+            <input type="text" />
+            <br />
+            <hr />
+            <button id="btn" class="btn btn-success">Sign In</button>
+            <br />
+            <h5>OR</h5>
+            <button id="btn" class="btn btn-success">Sign Up</button>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
 </template>
+<style>
+#btn {
+  width: 189px;
+  border-radius: 20px;
+}
+#box {
+    
+   
+}
+h5{
+    display: flex;
+    justify-content: center;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+</style>
